5f159cd78009ac2e6e874d497b11a31f672f87aa
Hikmet Melik Fırat <hikmetmelik@gmail.com>
2024-12-30 14:56:11 +0300

    Enhance CountVisualizer styling and add LandingView component for improved layout

--- src/components/CountVisualizer.tsx
+++ src/components/CountVisualizer.tsx
@@ -5,7 +5,9 @@ export default function CountVisualizer() {
 	const $count = useStore(count);
 	return (
 		<div>
-			<h1 className="text-2xl font-bold">Count Value : {$count}</h1>
+			<h1 className="text-2xl font-bold bg-gradient-to-r from-purple-500 to-blue-500 text-transparent bg-clip-text">
+				Count Value : {$count}
+			</h1>
 		</div>
 	);
 }
---
+++ src/components/LandingView.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import astroLogo from "../assets/astro.svg";
+</script>
+
+<template>
+	<div class="min-h-screen font-sans">
+		<main
+			class="container mx-auto min-h-screen flex items-center justify-center">
+			<div class="flex flex-col items-center justify-center gap-8 p-4">
+				<section class="text-center space-y-6">
+					<img
+						:src="astroLogo.src"
+						width="115"
+						height="48"
+						alt="Astro Homepage"
+						class="mx-auto" />
+					<div class="space-y-4">
+						<h1 class="text-3xl font-medium">
+							A simple example of using Astro with Vue and React.
+						</h1>
+						<p class="text-lg text-gray-500">
+							Click the buttons below to see the count increment.
+						</p>
+					</div>
+				</section>
+				<slot />
+			</div>
+		</main>
+	</div>
+</template>
