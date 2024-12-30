bf9b663c7cd9410462d96545c43bbb9b8979d25c
Hikmet Melik Fırat <hikmetmelik@gmail.com>
2024-12-30 15:20:11 +0300

    Refactor CountVisualizer and LandingView components for improved layout and styling

--- src/components/CountVisualizer.tsx
+++ src/components/CountVisualizer.tsx
@@ -4,8 +4,11 @@ import { count } from "../stores/count.ts";
 export default function CountVisualizer() {
 	const $count = useStore(count);
 	return (
-		<div>
-			<h1 className="text-2xl font-bold bg-gradient-to-r from-purple-500 to-blue-500 text-transparent bg-clip-text">
+		<div className="relative border-2 border-purple-400 rounded-lg p-4 w-full max-w-md">
+			<span className="absolute -top-3 right-4 bg-white px-2 text-purple-500 font-semibold">
+				React
+			</span>
+			<h1 className="text-2xl text-center font-bold bg-gradient-to-r from-purple-500 to-blue-500 text-transparent bg-clip-text">
 				Count Value : {$count}
 			</h1>
 		</div>
--- src/components/LandingView.vue
+++ src/components/LandingView.vue
@@ -1,29 +1,36 @@
 <script setup lang="ts">
-import astroLogo from "../assets/astro.svg";
+import astroLogo from "../assets/astro.svg?url";
 </script>
 
 <template>
 	<div class="min-h-screen font-sans">
 		<main
 			class="container mx-auto min-h-screen flex items-center justify-center">
-			<div class="flex flex-col items-center justify-center gap-8 p-4">
-				<section class="text-center space-y-6">
-					<img
-						:src="astroLogo.src"
-						width="115"
-						height="48"
-						alt="Astro Homepage"
-						class="mx-auto" />
-					<div class="space-y-4">
-						<h1 class="text-3xl font-medium">
-							A simple example of using Astro with Vue and React.
-						</h1>
-						<p class="text-lg text-gray-500">
-							Click the buttons below to see the count increment.
-						</p>
-					</div>
-				</section>
-				<slot />
+			<div
+				class="relative border-2 border-blue-400 rounded-lg p-8 w-full max-w-4xl">
+				<span
+					class="absolute -top-3 right-4 bg-white px-2 text-blue-500 font-semibold"
+					>Vue</span
+				>
+				<div class="flex flex-col items-center justify-center gap-8">
+					<section class="text-center space-y-6">
+						<img
+							:src="astroLogo"
+							width="115"
+							height="48"
+							alt="Astro Homepage"
+							class="mx-auto" />
+						<div class="space-y-4">
+							<h1 class="text-3xl font-medium">
+								A simple example of using Astro with Vue and React.
+							</h1>
+							<p class="text-lg text-gray-500">
+								Click the buttons below to see the count increment.
+							</p>
+						</div>
+					</section>
+					<slot />
+				</div>
 			</div>
 		</main>
 	</div>
